0189450f14048cab5d0d1077a489c18f185b425e
Maxim <kreper12321@gmail.com>
2020-05-02 19:46:47 +0400

    constructor стабильная версия 1.0.0

--- constructorShips.js
+++ constructorShips.js
@@ -1,13 +1,14 @@
-"use strict"
+"use strict";
 
-let selectCell = "emplyCell.png"
+let selectCell = "emplyCell.png";
 let shipMapSave = {};
 const ySize = 20;
 const xSize = 20;
 const cellSize = 20;
+let backVersionMapShip = {};
 
-shipMapSave = JSON.parse('{"x0 y0":"armorPlateCell.png","x1 y1":"armorPlateCell.png","x0 y2":"armorPlateCell.png","x0 y3":"armorPlateCell.png","x0 y4":"armorPlateCell.png","x2 y2":"armorPlateCell.png","x2 y3":"armorPlateCell.png","x2 y4":"armorPlateCell.png","x0 y1":"engineLeft.png","x1 y2":"controlBlock.png","x1 y3":"fuelTank.png","x1 y4":"fuelTank.png","x1 y5":"defualtCell.png","x2 y5":"fuelTank.png","x3 y5":"engineTop.png","x6 y5":"engineTop.png","x4 y6":"gun.png","x5 y6":"gun.png","x6 y6":"gunArmor.png","x3 y6":"gunArmor.png","x2 y6":"defualtCell.png","x7 y6":"defualtCell.png","x8 y5":"defualtCell.png","x7 y5":"fuelTank.png","x8 y4":"fuelTank.png","x8 y3":"fuelTank.png","x7 y4":"armorPlateCell.png","x7 y3":"armorPlateCell.png","x7 y2":"armorPlateCell.png","x9 y4":"armorPlateCell.png","x9 y3":"armorPlateCell.png","x9 y2":"armorPlateCell.png","x8 y1":"armorPlateCell.png","x9 y0":"armorPlateCell.png","x9 y1":"engineRight.png","x8 y2":"controlBlock.png","x7 y7":"engineBottom.png","x2 y7":"engineBottom.png","x6 y7":"fuelTank.png","x5 y7":"fuelTank.png","x4 y7":"fuelTank.png","x3 y7":"fuelTank.png","x4 y8":"engineBottom.png","x5 y8":"engineBottom.png","x3 y8":"engineLeft.png","x6 y8":"engineRight.png"}')
-shipMapSave = {"x10 y9":"armorPlateCell.png","x9 y9":"armorPlateCell.png","x10 y10":"armorPlateCell.png","x10 y11":"armorPlateCell.png","x9 y11":"armorPlateCell.png","x8 y9":"armorPlateCell.png","x8 y10":"armorPlateCell.png","x8 y11":"armorPlateCell.png","x9 y10":"fuelTank.png"};
+//shipMapSave = JSON.parse('{"x0 y0":"armorPlateCell.png","x1 y1":"armorPlateCell.png","x0 y2":"armorPlateCell.png","x0 y3":"armorPlateCell.png","x0 y4":"armorPlateCell.png","x2 y2":"armorPlateCell.png","x2 y3":"armorPlateCell.png","x2 y4":"armorPlateCell.png","x0 y1":"engineLeft.png","x1 y2":"controlBlock.png","x1 y3":"fuelTank.png","x1 y4":"fuelTank.png","x1 y5":"defualtCell.png","x2 y5":"fuelTank.png","x3 y5":"engineTop.png","x6 y5":"engineTop.png","x4 y6":"gun.png","x5 y6":"gun.png","x6 y6":"gunArmor.png","x3 y6":"gunArmor.png","x2 y6":"defualtCell.png","x7 y6":"defualtCell.png","x8 y5":"defualtCell.png","x7 y5":"fuelTank.png","x8 y4":"fuelTank.png","x8 y3":"fuelTank.png","x7 y4":"armorPlateCell.png","x7 y3":"armorPlateCell.png","x7 y2":"armorPlateCell.png","x9 y4":"armorPlateCell.png","x9 y3":"armorPlateCell.png","x9 y2":"armorPlateCell.png","x8 y1":"armorPlateCell.png","x9 y0":"armorPlateCell.png","x9 y1":"engineRight.png","x8 y2":"controlBlock.png","x7 y7":"engineBottom.png","x2 y7":"engineBottom.png","x6 y7":"fuelTank.png","x5 y7":"fuelTank.png","x4 y7":"fuelTank.png","x3 y7":"fuelTank.png","x4 y8":"engineBottom.png","x5 y8":"engineBottom.png","x3 y8":"engineLeft.png","x6 y8":"engineRight.png"}');
+//shipMapSave = {"x10 y9":"armorPlateCell.png","x9 y9":"armorPlateCell.png","x10 y10":"armorPlateCell.png","x10 y11":"armorPlateCell.png","x9 y11":"armorPlateCell.png","x8 y9":"armorPlateCell.png","x8 y10":"armorPlateCell.png","x8 y11":"armorPlateCell.png","x9 y10":"fuelTank.png"};
 
 
 function generateCells(ySize, xSize, cellSize, scale, mapObjects) {
@@ -44,17 +45,18 @@ function generateCells(ySize, xSize, cellSize, scale, mapObjects) {
     return code;
 }
 
-document.body.innerHTML += `<div class="menuOfSelect" style="position: absolute; right:500px;">
-    <div class="armorPlateCell"><img src="img/armorPlateCell.png" alt="armorPlateCell" height=" 50px;" class="armorPlateCell">armorPlateCell</div>
-    <div class="defualtCell"><img src="img/defualtCell.png" alt="defualtCell" height=" 50px;" class="defualtCell">defualtCell</div>
-    <div class="fuelTank"><img src="img/fuelTank.png" alt="fuelTank" height=" 50px;" class="fuelTank">fuelTank</div>
-    <div class="gun"><img src="img/gun.png" alt="gun" height=" 50px;" class="gun">gun</div>
-    <div class="gunArmor"><img src="img/gunArmor.png" alt="gunArmor" height=" 50px;" class="gunArmor">gunArmor</div>
-    <div class="controlBlock"><img src="img/controlBlock.png" alt="controlBlock" height=" 50px;" class="controlBlock">controlBlock</div>
-    <div class="engine"><img src="img/engineTop.png" alt="engine" height=" 50px;" class="engine">engine</div>
-    </div>`;
+document.querySelector(".menuOfSelect").innerHTML += `
+    <div class="armorPlateCell"><img src="img/armorPlateCell.png" alt="armorPlateCell" height=" 50px;" class="armorPlateCell">Защитная пластина. Прочность 200. Масса 100</div>
+    <div class="defualtCell"><img src="img/defualtCell.png" alt="defualtCell" height=" 50px;" class="defualtCell">Обшивка. Прочность 80. Масса 10</div>
+    <div class="fuelTank"><img src="img/fuelTank.png" alt="fuelTank" height=" 50px;" class="fuelTank">Топливный бак. Прочность 10. При уничтожении наносит 5 урона ближайшим блокам.Масса 100</div>
+    <div class="gun"><img src="img/gun.png" alt="gun" height=" 50px;" class="gun">Оружие.Прочность 100. Урон 200 за выстрел.Масса 70</div>
+    <div class="gunArmor"><img src="img/gunArmor.png" alt="gunArmor" height=" 50px;" class="gunArmor">Боезапас. Прочность 50. При уничтожениее нанаосит ближайшим блокам 200 урона.Масса 80</div>
+    <div class="controlBlock"><img src="img/controlBlock.png" alt="controlBlock" height=" 50px;" class="controlBlock">Командная рубка(лимит 3). Прочность 200. Масса 250. При уничтожении всех командных рубок кораболь самоуничтожается</div>
+    <div class="engine"><img src="img/engineTop.png" alt="engine" height=" 50px;" class="engine">Двигатель. Прочность 50. Масса 25. Тяга 2.5.</div>
+    `;
 
-document.querySelector(".constructorMatrice").innerHTML = generateCells(ySize, xSize, cellSize, 0, 1213)
+document.querySelector(".constructorMatrice").innerHTML = generateCells(ySize, xSize, cellSize, 0, 1213);
+shiftCenterConstructor();
 
 document.querySelector(".constructorMatrice").addEventListener("click", event => {
     if (event.target.style.backgroundImage === 'url("img/emplyCell.png")') {
@@ -63,11 +65,11 @@ document.querySelector(".constructorMatrice").addEventListener("click", event =>
             shipMapSave[event.target.getAttribute("class")] = selectCell;
         }
     } else  {
-        event.target.style.backgroundImage = 'url("img/emplyCell.png")'
+        event.target.style.backgroundImage = 'url("img/emplyCell.png")';
         delete shipMapSave[event.target.getAttribute("class")];
     }
 
-})
+});
 
 //defualtCell.png
 //img/armorPlateCell.png
@@ -80,10 +82,10 @@ function contextMenuForEngine(open) {
     if (open) {
         document.querySelector(".menuOfSelect").innerHTML += `
     <div class="contextMenuForEngine" style=" position: absolute; right: -40px; bottom: 0;">
-        <div class="engineTop"><img src="img/engineTop.png" alt="engineTop" height=" 50px;" class="engineTop">gunArmor</div>
-        <div class="engineRight"><img src="img/engineRight.png" alt="engineRight" height=" 50px;" class="engineRight">gunArmor</div>
-        <div class="engineBottom"><img src="img/engineBottom.png" alt="engineBottom" height=" 50px;" class="engineBottom">gunArmor</div>
-        <div class="engineLeft"><img src="img/engineLeft.png" alt="engineLeft" height=" 50px;" class="engineLeft">gunArmor</div>
+        <div class="engineTop"><img src="img/engineTop.png" alt="engineTop" height=" 50px;" class="engineTop">Двигатель направленный вверх</div>
+        <div class="engineRight"><img src="img/engineRight.png" alt="engineRight" height=" 50px;" class="engineRight">Двигатель направленный вправо</div>
+        <div class="engineBottom"><img src="img/engineBottom.png" alt="engineBottom" height=" 50px;" class="engineBottom">Двигатель направленный вниз</div>
+        <div class="engineLeft"><img src="img/engineLeft.png" alt="engineLeft" height=" 50px;" class="engineLeft">Двигатель направленный влево</div>
     </div>
     `;
     }
@@ -94,7 +96,7 @@ function contextMenuForEngine(open) {
 }
 
 document.querySelector(".menuOfSelect").addEventListener("click", event => {
-    console.log(true)
+    console.log(true);
     contextMenuForEngine(false);
     if ("defualtCell" === event.target.getAttribute("class")) {
         selectCell = "defualtCell.png";
@@ -138,7 +140,7 @@ document.querySelector(".menuOfSelect").addEventListener("click", event => {
         selectCell = "engineLeft.png";
         document.querySelector(".menuOfSelect .engine img").setAttribute("src", "img/engineLeft.png")
     }
-})
+});
 
 
 function getCodeOfSaveMapShip() {
@@ -154,20 +156,20 @@ function findNumberInString(str) {
             if (str[i + 2] === "y") {
                 numbersForReturn.x = (Number(str[i]))
             } else if (str[i + 3] === "y") {
-                numbersForReturn.x = (Number(str[i] + str[i + 1]))
+                numbersForReturn.x = (Number(str[i] + str[i + 1]));
                 i++
             } else if (str[i + 4] === "y") {
-                numbersForReturn.x = (Number(str[i] + str[i + 1] +  str[i + 2]))
+                numbersForReturn.x = (Number(str[i] + str[i + 1] +  str[i + 2]));
                 i += 2
             }else if (str[i + 1] === "y") {
                 i++;
             } else if (str[i + 1] === undefined) {
                 numbersForReturn.y = (Number(str[i]))
             } else if (str[i + 2] === undefined) {
-                numbersForReturn.y = (Number(str[i] + str[i + 1]))
+                numbersForReturn.y = (Number(str[i] + str[i + 1]));
                 i += 2;
             } else if (str[i + 3] === undefined) {
-                numbersForReturn.y = (Number(str[i] + str[i + 1] + str[i += 2]))
+                numbersForReturn.y = (Number(str[i] + str[i + 1] + str[i += 2]));
                 i += 2;
             }
         }
@@ -176,6 +178,11 @@ function findNumberInString(str) {
 }
 
 function ShiftShip(deviationByX, deviationByY) {
+    backVersionMapShip = {};
+    for (let key in shipMapSave) {
+        backVersionMapShip[key] = shipMapSave[key];
+
+    }
     let setEdition = new Set();
     let arrOriginal = [];
     for (let key in shipMapSave) {
@@ -185,9 +192,13 @@ function ShiftShip(deviationByX, deviationByY) {
     let copyOriginalObj = Object.assign({}, shipMapSave);
     for (let key in shipMapSave) {
         let valueProperty = copyOriginalObj[key];
-        if (key === "x9 y10") debugger;
-        //Все ламается потому, что у тебя при здвиге первее сдвигается боковая клетка и от того инфао прошлой клетке перезаписывется . Сейчас думаю можно сделать объект с свойствами, что будут переименновываться на 1 но при этом его значение не затрагивается.
         let coordinates = findNumberInString(key);
+        if (coordinates.x - deviationByX < 0) {
+            return;
+        }
+        if (coordinates.y - deviationByY < 0) {
+            return;
+        }
         shipMapSave[`x${coordinates.x - deviationByX} y${coordinates.y - deviationByY}`] = valueProperty;
         setEdition.add(`x${coordinates.x - deviationByX} y${coordinates.y - deviationByY}`);
     }
@@ -204,8 +215,6 @@ function ShiftShip(deviationByX, deviationByY) {
 }
 
 function alignmentShip() {
-    let deviationByX = 0;
-    let deviationByY = 0;
     let arrXShip = [];
     let arrYShip = [];
 
@@ -213,7 +222,7 @@ function alignmentShip() {
         if (a > b) return 1;
         if (a == b) return 0;
         if (a < b) return -1;
-    }
+    };
 
     for (let key in shipMapSave) {
         arrXShip.push(findNumberInString(key).x);
@@ -221,23 +230,98 @@ function alignmentShip() {
     }
 
 
-    deviationByX = arrXShip.sort(compareNumeric)[0];
-    deviationByY = arrYShip.sort(compareNumeric)[0];
+    let deviationByX = arrXShip.sort(compareNumeric)[0];
+    let deviationByY = arrYShip.sort(compareNumeric)[0];
 
-    ShiftShip(deviationByX, deviationByY)
+    ShiftShip(deviationByX, deviationByY);
 
     document.querySelector(".constructorMatrice").innerHTML = generateCells(ySize, xSize, cellSize, 0, 1213)
 }
 
+function shipDimensions(ship) {
+    let arrXShip = [];
+    let arrYShip = [];
+    let size = {};
+    let centerShip = {};
+    let compareNumeric = function (a, b) {
+        if (a > b) return 1;
+        if (a == b) return 0;
+        if (a < b) return -1;
+    };
+    for (let key in ship) {
+        arrXShip.push(findNumberInString(key).x);
+        arrYShip.push(findNumberInString(key).y);
+
+    }
+
+    if (arrXShip.length === 1) {
+        size.x = 1;
+    } else {
+        arrXShip = arrXShip.sort(compareNumeric);
+        size.x = [arrXShip[arrXShip.length - 1]] - arrXShip[0] + 1;
+    }
+
+    if (arrYShip.length === 1) {
+        size.y = 1;
+    } else {
+        arrYShip = arrYShip.sort(compareNumeric);
+        size.y = [arrYShip[arrYShip.length - 1]] - arrYShip[0] + 1;
+    }
+
+    centerShip.x = arrXShip[0] + Number((size.x / 2).toFixed(0));
+    centerShip.y = arrYShip[0] + Number((size.y / 2).toFixed(0));
+
+    let dateOfReturn = {};
+    dateOfReturn.x = size.x
+    dateOfReturn.y = size.y
+    dateOfReturn.center = {x: centerShip.x, y: centerShip.y,}
+    return dateOfReturn;
+}
+
+function actionBack() {
+    shipMapSave = {};
+    for (let key in backVersionMapShip) {
+        shipMapSave[key] = backVersionMapShip[key ];
+    }
+    document.querySelector(".constructorMatrice").innerHTML = generateCells(ySize, xSize, cellSize, 0, 1213)
+}
+
+function shiftCenterConstructor() {
+    let sizeShip = shipDimensions(shipMapSave);
+    let shiftX = (xSize / 2).toFixed(0) - sizeShip.center.x;
+    let shiftY = (ySize / 2).toFixed(0) - sizeShip.center.y;
+    ShiftShip(-shiftX, -shiftY)
+}
+
+function closeContructor() {
+    document.querySelector(".constructorWindow").style.display = "none";
+}
+
+function saveShip() {
+    document.querySelector(".blockOfSave").innerHTML = JSON.stringify(shipMapSave);
+}
+
 document.querySelector(".shiftTopConstructor").addEventListener("click", () => {
-    ShiftShip(0, 1);
-})
-document.querySelector(".shiftRightConstructor").addEventListener("click", () => {
-    ShiftShip(-1, 0);
-})
-document.querySelector(".shiftBottomConstructor").addEventListener("click", () => {
     ShiftShip(0, -1);
-})
-document.querySelector(".shiftLeftConstructor").addEventListener("click", () => {
+});
+document.querySelector(".shiftRightConstructor").addEventListener("click", () => {
     ShiftShip(1, 0);
+});
+document.querySelector(".shiftBottomConstructor").addEventListener("click", () => {
+    ShiftShip(0, 1);
+});
+document.querySelector(".shiftLeftConstructor").addEventListener("click", () => {
+    ShiftShip(-1, 0);
+});
+document.querySelector(".shiftCenterConstructor").addEventListener("click", () => {
+    shiftCenterConstructor();
+});
+document.querySelector(".closeContructor").addEventListener("click", () => {
+    closeContructor();
 })
+document.querySelector(".BTNOFSAVE").addEventListener("click", () => {
+    saveShip();
+})
+
+
+
