8493f469bf016de21fdce984f8c1e9fbd59b0e1f
K1ngVlad <kingvlad3008@gmail.com>
2020-12-21 18:11:39 +0400

    tutorial.js

---
+++ Vlad/tutorial.js
@@ -0,0 +1,19 @@
+console.log('Ты меня видишь?');
+console.log('Вот я тебя вижу');
+console.log(`Вот тебе все типы данных`);
+console.log(
+  88005553535,
+  3.14,
+  95 * 'а нам всё равно а нам всё равннооооо!!!',
+  7 / 0
+);
+console.log(1 == 2 - 1, 3 == 0);
+console.log(null);
+console.log(undefined);
+console.log(Symbol());
+console.log({ name: 'Vlad', age: 18, intelligence: 2 / 0 });
+
+//Окей, давай продолжим
+var name = 'Стандартный Влад';
+let $name = 'Стабильный Влад...';
+const name2002 = 'Постоянный Влад';
