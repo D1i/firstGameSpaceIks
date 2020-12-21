48a18d2accdb928aa43c8b80560ea5461465baa2
K1ngVlad <kingvlad3008@gmail.com>
2020-12-21 17:59:14 +0400

    Типы данных

---
+++ Vlad/code.js
@@ -0,0 +1,14 @@
+console.log('Ты меня видишь?');
+console.log('Вот я тебя вижу');
+console.log('Вот тебе все типы данных');
+console.log(
+  88005553535,
+  3.14,
+  95 * 'а нам вс равно а нам всё равннооооо!!!',
+  7 / 0
+);
+console.log(1 == 2 - 1, 3 == 0);
+console.log(null);
+console.log(undefined);
+console.log(Symbol());
+console.log({ name: 'Vlad', age: 18, intelligence: 2 / 0 });
